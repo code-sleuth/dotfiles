7309d8df04dd51e41785193fd06c78a4a02b70e4
Ibrahim Mbaziira <code.ibra@gmail.com>
2025-09-03 15:01:10 +0300

    style: 🎨 use jetbrains mono nerd font
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,6 +4,20 @@
 vim.opt.wrap = true
 vim.g.codeium_os = "Darwin"
 vim.g.codeium_arch = "arm64"
+
+-- Font configuration for GUI Neovim clients (neovide, etc.)
+if vim.g.neovide then
+  vim.g.neovide_font_family = "JetBrainsMono Nerd Font"
+  vim.g.neovide_font_size = 14
+  vim.g.neovide_padding_top = 10
+  vim.g.neovide_padding_bottom = 10
+  vim.g.neovide_padding_right = 10
+  vim.g.neovide_padding_left = 10
+end
+
+-- Font configuration for other GUI clients
+vim.opt.guifont = "JetBrainsMono Nerd Font:h14"
+
 -- LSP Server to use for Rust.
 -- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
 -- only for diagnostics. The rest of LSP support will still be
