463d00714f6be3ae70346fe04060efafaa1e7a24
Ibrahim Mbaziira <code.ibra@gmail.com>
2025-09-10 04:31:07 +0300

    chore: 🔧 switch to homemanager for handling dotfiles
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -1,8 +1,6 @@
 # home.nix
 # home-manager switch
 
-# { config, pkgs, ... }:
-
 {
   home.username = "code";
   home.homeDirectory = "/Users/code";
@@ -15,6 +13,28 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".zshrc".source = "~/dotfiles/zsh/zshrc";
+    ".zprofile".source = "~/dotfiles/zsh/zprofile";
+    ".config/wezterm".source = "~/dotfiles/wezterm";
+    ".config/skhd".source = "~/dotfiles/skhd";
+    ".config/starship".source = "~/dotfiles/starship";
+    ".config/zellij".source = "~/dotfiles/zellij";
+    ".config/nvim".source = "~/dotfiles/nvim";
+    ".config/nix".source = "~/dotfiles/nix";
+    ".config/nix-darwin".source = "~/dotfiles/nix-darwin";
+    ".config/tmux".source = "~/dotfiles/tmux";
+    ".config/ghostty".source = "~/dotfiles/ghostty";
+    ".config/aerospace".source = "~/dotfiles/aerospace";
+    ".config/sketchybar".source = "~/dotfiles/sketchybar";
+    ".config/nushell".source = "~/dotfiles/nushell";
+    ".config/git".source = " ~/dotfiles/git";
+    ".config/bat".source = "~/dotfiles/bat";
+    ".config/atuin".source = "~/dotfiles/atuin";
+    ".config/ledger".source = "~/dotfiles/ledger";
+    ".lesskey".source = "~/dotfiles/less";
+    ".config/neofetch".source = "~/dotfiles/neofetch";
+    ".config/raycast".source = "~/dotfiles/raycast";
+    ".config/thefuck".source = "~/dotfiles/thefuck";
   };
 
   home.sessionVariables = {
@@ -26,15 +46,15 @@
     "/run/current-system/sw/bin"
     "$HOME/.nix-profile/bin"
   ];
-  programs.home-manager.enable = false; # disable home manager
-  # programs.zsh = {
-  #   enable = true;
-  #   initExtra = ''
-  #     # Add any additional configurations here
-  #     export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
-  #     if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-  #       . '/nix/var/nix/profiles/default/etc/profile.d/nix-darwin.sh'
-  #     fi
-  #   '';
-  # };
+  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      # Add any additional configurations here
+      export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-darwin.sh'
+      fi
+    '';
+  };
 }
