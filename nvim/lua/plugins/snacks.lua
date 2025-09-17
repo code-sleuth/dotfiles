5584939211edf3ea787ccfa119cc720eab0e5a18
Ibrahim Mbaziira <code.ibra@gmail.com>
2025-09-17 18:30:14 +0300

    chore: 🔧 show hidden files in snacks file tree
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,27 +1,41 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-        explorer = {
-            enabled = true,
-        },
-        picker = {
-            layouts = {
-                sidebar = {
-                    layout = {
-                        position = "right",
-                    },
-                },
-            },
-        },
-        bigfile = { enabled = true },
-        dashboard = { enabled = true },
-        indent = { enabled = true },
-        input = { enabled = true },
-        notifier = { enabled = true },
-        quickfile = { enabled = true },
-        scope = { enabled = true },
-        scroll = { enabled = true },
-        statuscolumn = { enabled = true },
-        words = { enabled = true },
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      enabled = true,
     },
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+        grep = {
+          hidden = true,
+          ignored = true,
+        },
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+      layouts = {
+        sidebar = {
+          layout = {
+            position = "right",
+          },
+        },
+      },
+    },
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
 }
