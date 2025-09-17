7e04108662deed86dda3782b7a75119d43f6eebb
Ibrahim Mbaziira <code.ibra@gmail.com>
2025-09-17 18:35:15 +0300

    style: 🎨 change color scheme
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- nvim/lua/plugins/rosepine.lua
+++ nvim/lua/plugins/rosepine.lua
@@ -47,8 +47,66 @@ return {
             },
 
             highlight_groups = {
-                -- Comment = { fg = "foam" },
-                -- VertSplit = { fg = "muted", bg = "muted" },
+                -- Keywords and control flow (purple/mauve)
+                ["@keyword"] = { fg = "#c6a0f6" }, -- Catppuccin mauve
+                ["@keyword.function"] = { fg = "#c6a0f6" },
+                ["@keyword.operator"] = { fg = "#c6a0f6" },
+                ["@conditional"] = { fg = "#c6a0f6" },
+                ["@repeat"] = { fg = "#c6a0f6" },
+                ["@exception"] = { fg = "#c6a0f6" },
+
+                -- Functions (blue)
+                ["@function"] = { fg = "#8aadf4" }, -- Catppuccin blue
+                ["@function.call"] = { fg = "#8aadf4" },
+                ["@function.builtin"] = { fg = "#8aadf4" },
+                ["@method"] = { fg = "#8aadf4" },
+                ["@method.call"] = { fg = "#8aadf4" },
+
+                -- Strings (green)
+                ["@string"] = { fg = "#a6da95" },
+                ["@string.escape"] = { fg = "#f5a97f" },
+                ["@string.regex"] = { fg = "#a6da95" },
+
+                -- Numbers and booleans (peach/orange)
+                ["@number"] = { fg = "#f5a97f" },
+                ["@float"] = { fg = "#f5a97f" },
+                ["@boolean"] = { fg = "#f5a97f" },
+
+                -- Variables and identifiers
+                ["@variable"] = { fg = "text" },
+                ["@variable.builtin"] = { fg = "#ed8796" },
+                ["@parameter"] = { fg = "text" },
+
+                -- Types (yellow)
+                ["@type"] = { fg = "#eed49f" },
+                ["@type.builtin"] = { fg = "#eed49f" },
+                ["@type.definition"] = { fg = "#eed49f" },
+
+                -- Properties and fields
+                ["@property"] = { fg = "text" },
+                ["@field"] = { fg = "text" },
+
+                -- Operators
+                ["@operator"] = { fg = "#91d7e3" },
+
+                -- Constants
+                ["@constant"] = { fg = "#f5a97f" },
+                ["@constant.builtin"] = { fg = "#f5a97f" },
+
+                -- Comments (keep Rose Pine style but enhance)
+                ["@comment"] = { fg = "muted", italic = true },
+
+                -- Punctuation
+                ["@punctuation.bracket"] = { fg = "subtle" },
+                ["@punctuation.delimiter"] = { fg = "subtle" },
+
+                -- Tags (for markup languages)
+                ["@tag"] = { fg = "#f5bde6" },           -- Catppuccin pink
+                ["@tag.attribute"] = { fg = "#eed49f" }, -- Catppuccin yellow
+
+                -- Special
+                ["@constructor"] = { fg = "#8aadf4" }, -- Catppuccin blue
+                ["@namespace"] = { fg = "#eed49f" },   -- Catppuccin yellow
             },
         },
     },
