aa199234ce8f82b6afaad723ac8aa3b97f1e1f8a
Ibrahim Mbaziira <code.ibra@gmail.com>
2026-08-14 01:46:44 +0300

    refactor(git): move signing identity to per-host file
    - shared ~/.config/git is a read-only store symlink, so identity cannot be overridden per machine
    
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- nix-darwin/hosts/darwin/Ibrahims-MBP/default.nix
+++ nix-darwin/hosts/darwin/Ibrahims-MBP/default.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ username, ... }:
 {
   nix.enable = false;
+
+  home-manager.users.${username}.home.file.".config/git-identity".text = ''
+    [user]
+        email = dev.mbaziira@gmail.com
+        signingkey = dev.mbaziira@gmail.com
+  '';
 
   system.defaults = {
     WindowManager = {
--- nix-darwin/hosts/darwin/Ibrahims-Thanos/default.nix
+++ nix-darwin/hosts/darwin/Ibrahims-Thanos/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ username, ... }:
 {
   # Host-specific configuration for Ibrahims-Thanos
 
@@ -6,6 +6,12 @@
     ./packages.nix
     ./homebrew.nix
   ];
+
+  home-manager.users.${username}.home.file.".config/git-identity".text = ''
+    [user]
+        email = code.ibra@gmail.com
+        signingkey = 90CEC26C2DA90D65A751ECED9E900E9767E815A0
+  '';
 
   # Disable widgets and Stage Manager
   system.defaults = {
